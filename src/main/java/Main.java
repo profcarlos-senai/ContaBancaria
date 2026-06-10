0dcac18ccf96cf35841a09b2df409577866a1dcd
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-06-09 21:06:47 -0300

    aula de 09/06

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
         // cria um objeto da classe ContaBancaria e guarda em minhaGrana
         ContaBancaria minhaGrana = new ContaBancaria();
         minhaGrana.depositar(500);
-        System.out.println("(Grana) Tenho R$ " + minhaGrana.getSaldo() );
-
+        System.out.println("Tenho R$ " + minhaGrana.getSaldo() );
+        minhaGrana.sacar(100);
+        System.out.println("Sobrou R$ " + minhaGrana.getSaldo() );
     }
 }
